7a9995101aeb6418008eb1442422c102b4be316b
hut <hut@lepus.uberspace.de>
2016-02-02 04:29:21 +0100

    added crude bash tab completion

---
+++ bash_tab_completion.sh
@@ -0,0 +1,25 @@
+_exobrain_walkdir() {
+	local namestart="$1"
+	local dir="$2"
+
+	# Append / to directories to get tab completions of the files inside
+	if [ -d "$dir/$namestart" ]; then
+		namestart="$namestart/"
+	fi
+
+	finddir="$(dirname "${namestart%%/*}")"
+	finddir="${finddir:-.}"
+	basename="${namestart##*/}"
+
+	cd "$dir"
+
+	find "$finddir" -name "$basename*" -a \! -name '.git*' | sed 's!./!!'
+	find "$finddir" -type f -name "$basename*" -a \! -name '.git*' | sed 's!./!!' | xargs basename -a 2> /dev/null
+}
+
+_TabComplete_Exobrain () {
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+	COMPREPLY=( $(compgen -W "$(_exobrain_walkdir "$cur" "$EXOBRAIN_ROOT")" "$cur" ) )
+}
+
+complete -F _TabComplete_Exobrain -o filenames eb
